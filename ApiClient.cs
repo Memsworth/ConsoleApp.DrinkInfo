9a714cf8334ce98800bf215acd0e2f061f4e98e0
Abubakar Ahmed <abubakarahmed4497@outlook.com>
2023-02-16 20:58:42 +0900

    base classes are working correctly

---
+++ ApiClient.cs
@@ -0,0 +1,13 @@
+﻿public static class Client
+{
+    public static HttpClient ApiClient { get; private set; }
+
+    public static void InitializeClient(string url)
+    {
+        ApiClient = new HttpClient
+        {
+            BaseAddress = new Uri(url)
+        };
+        ApiClient.DefaultRequestHeaders.Clear();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text.Json;
+
+using HttpClient client = new HttpClient
+{
+    BaseAddress = new Uri("https://www.thecocktaildb.com/api/json/v1/1/")
+};
+client.DefaultRequestHeaders.Accept.Clear();
+await GetData(client);
+
+
+
+
+
+static async Task GetData(HttpClient client)
+{
+    await using Stream stream = await client.GetStreamAsync("list.php?c=list");
+
+    var data = await JsonSerializer.DeserializeAsync<RootObject>(stream);
+
+    var categories = data.drinks;
+
+    foreach (var item in categories)
+    {
+        Console.WriteLine(item.strCategory.ToString());
+    }
+
+}
+
+
+public class RootObject
+{
+    public List<Drinks> drinks { get; set; }
+}
+
+public class Drinks
+{ 
+    public string strCategory { get; set; }
+}
+
+
+
